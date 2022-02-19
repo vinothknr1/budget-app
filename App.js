845267da885c5bf513dd35fd608e1ca8c39dfc00
VinothKumar <89235649+vinowoualabs@users.noreply.github.com>
2022-02-19 18:01:53 +0530

    Budget App Basic Setup

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Budget App Basic Code </Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
