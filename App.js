a973c81513c796056f7eebe2d05a8e83e09ec0a8
VinothKumar <vinothknr1@gmail.com>
2022-02-20 12:16:07 +0530

    App Icon and Splash Screen Completed

--- App.js
+++ App.js
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Budget App Basic Code </Text>
+      <Text>Budget App Basic Code (Production Mode) </Text>
       <StatusBar style='auto' />
     </View>
   );
