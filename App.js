4f37614226dffffef3a1b6bb41f170c083208233
VinothKumar <vinothknr1@gmail.com>
2022-02-21 13:15:20 +0530

    Poppins Font Loaded Sucessfully

--- App.js
+++ App.js
@@ -1,13 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+import { useState } from 'react';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    popregular: require('./fonts/Poppins-Regular.ttf'),
+    popbold: require('./fonts/Poppins-Bold.ttf'),
+    popblack: require('./fonts/Poppins-Black.ttf'),
+  });
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Budget App Basic Code (Production Mode) </Text>
-      <StatusBar style='auto' />
-    </View>
-  );
+  const [fontLoaded, setFontLoaded] = useState(false);
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => {
+          setFontLoaded(true);
+        }}
+        onError={(err) => {
+          console.log(err);
+        }}
+      />
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Budget App Basic Code (Production Mode){' '}
+        </Text>
+        <StatusBar style='auto' />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +44,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 16,
+    fontFamily: 'popbold',
   },
 });
